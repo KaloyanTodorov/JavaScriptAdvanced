e6001125fd3cc928d3a87b77acdd5e1828b90630
Kaloyan Todorov <kaloyantodorov@Renetas-MacBook-Pro.local>
2018-09-10 01:22:49 +0300

    Added jQuery Calendar

--- Exercises3 - Advanced Functions/01.SortArray.js
+++ Exercises3 - Advanced Functions/01.SortArray.js
@@ -1,3 +1,11 @@
-function (arr, sortMethod) {
+function sortArray(arr, sortMethod) {
+    let ascComparator = (a, b) => a - b;
+    let descComparator = (a, b) => b - a;
 
+    let sortingStrategies = {
+        asc: ascComparator,
+        desc: descComparator
+    }
+
+    return arr.sort(sortingStrategies[sortMethod]);
 }
